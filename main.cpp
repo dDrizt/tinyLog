bac221250cb07102de7032f834fdc7903fd19c77
kiril <q598794700@gmail.com>
2023-08-26 11:29:23 +0800

    Color with ANSI

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "tinylog.h"
 
 int main() {
-    tinylog::log_debug("hello");
+    tinylog::log_info("the answer is {}", 42);
+
+    #define _FUNCTION(name) tinylog::log_##name(#name);
+        TINYLOG_FOREACH_LOG_LEVEL(_FUNCTION)
+    #undef _FUNCTION
+    
 
     return 0;
 }
--- tinylog.h
+++ tinylog.h
@@ -6,6 +6,7 @@
 #include <cstdint>
 #include <type_traits>
 #include <fstream>
+#include <chrono>
 
 namespace tinylog {
 
@@ -25,6 +26,32 @@ enum class log_level : std::uint8_t {
 };
 
 namespace details {
+
+#if defined (__linux__) || defined (__APPLE__)
+inline constexpr char k_level_ansi_colors[(std::uint8_t)log_level::fatal + 1][8] = {
+    "\E[37m",
+    "\E[35m",
+    "\E[32m",
+    "\E[34m",
+    "\E[33m",
+    "\E[31m",
+    "\E[31;1m",
+};
+inline constexpr char k_reset_ansi_color[4] = "\E[m";
+#define _TINYLOG_IF_HAS_ANSI_COLORS(x) x
+#else
+#define _TINYLOG_IF_HAS_ANSI_COLORS(x)
+inline constexpr char k_level_ansi_colors[(std::uint8_t)log_level::fatal + 1][1] = {
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+};
+inline constexpr char k_reset_ansi_colors[1] = "";
+#endif
 
 inline std::string log_level_name(log_level lev) {
     switch (lev) {
@@ -63,9 +90,30 @@ inline log_level g_max_level = [] () -> log_level {
     return log_level::info;
 } ();
 
-inline std::ofstream g_log_file("tiny.log");
+inline std::ofstream g_log_file = [] () -> std::ofstream {
+    if (auto path = std::getenv("TINYLOG_FILE")) {
+        return std::ofstream(path, std::ios::app);
+    }
+    return std::ofstream();
+} ();
+
+inline void output_log(log_level lev, std::string msg, std::source_location const &loc) {
+    std::chrono::zoned_time now{std::chrono::current_zone(), std::chrono::high_resolution_clock::now()};
+    msg = std::format("{} {}:{} [{}] {}", now, loc.file_name(), loc.line(), details::log_level_name(lev), msg);
+    if (g_log_file) {
+        g_log_file << msg + '\n';
+    }
+    if (lev >= g_max_level) {
+        std::cout << _TINYLOG_IF_HAS_ANSI_COLORS(k_level_ansi_colors[(std::uint8_t)lev] +)
+                                                 msg _TINYLOG_IF_HAS_ANSI_COLORS(+ k_reset_ansi_color) + '\n';
+    }
+}
 
 } // namespace details
+
+inline void set_log_file(std::string path) {
+    details::g_log_file = std::ofstream(path, std::ios::app);
+}
 
 inline void set_log_level(log_level lev) {
     details::g_max_level = lev;
@@ -76,7 +124,13 @@ template<typename... Args>
 void generic_log(log_level lev, details::with_source_location<std::format_string<Args...>> fmt, Args &&...args) {
     if (lev >= details::g_max_level) {
         auto const &loc = fmt.location();
-        std::cout << loc.file_name() << ":" << loc.line() << " [" << details::log_level_name(lev) << "] " << std::vformat(fmt.format().get(), std::make_format_args(args...)) << '\n';
+        // std::cout TINYLOG_IF_HAS_ANSI_COLORS(<< "\E[" << details::level_ansi_colors[(std::uint8_t)lev])
+        //     << loc.file_name() << ":" << loc.line() << " [" << details::log_level_name(lev)<< "] "
+        //     << std::vformat(fmt.format().get(), std::make_format_args(args...))
+        //     TINYLOG_IF_HAS_ANSI_COLORS(<< "\E[m") << '\n';
+        auto msg = std::vformat(fmt.format().get(), std::make_format_args(args...));
+        details::output_log(lev, std::move(msg), loc); 
+        
     }
 }
 
